26a561850a8a285f9a6bef646e49ed1ead4ef13d
camagni paolo <paolo.camagni@magistricumacini.edu.it>
2023-06-15 07:26:48 +0000

    "secondo commit"

---
+++ somma .py
@@ -0,0 +1,5 @@
+#prova programma phyton
+numero1 = int(input("Inserisci il primo numero: "))
+numero2 = int(input("Inserisci il secondo numero: "))
+somma = numero1 + numero2
+print("La somma di", numero1, "+", numero2, "è uguale a:", somma)
